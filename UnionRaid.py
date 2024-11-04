d6d3ba2b483c7b4d23babd2e5305f4c161b985d6
Justin Lin <just435lin@gmail.com>
2024-11-04 08:52:53 -0600

    created UnionRaid class

---
+++ UnionRaid.py
@@ -0,0 +1,14 @@
+
+class UnionRaid:
+  name = ''
+  powerRank = []
+  bosses = []
+
+  spreadsheet = []
+
+  NUM_BOSS = 5
+  NUM_TIERS = 10
+  schedule = ['' for i in range(NUM_BOSS*NUM_TIERS)]
+
+
+  
